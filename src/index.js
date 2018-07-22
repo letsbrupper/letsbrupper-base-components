5de775ad055e2f80ae5adbd8c2d827a6e21682b9
thanhj <johnky.nguyen@gmail.com>
2018-07-22 22:54:34 +0800

    Update list of components in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import App from "./App";
 import style from "./main.css";
+import Button from './components/Button';
+import BoxDisplay from './components/BoxDisplay';
+import Column from './components/Column';
+import Grid from './components/Grid';
+import Row from './components/Row';
+
+export {
+  Button,
+  BoxDisplay,
+  Column,
+  Grid,
+  Row,
+}
